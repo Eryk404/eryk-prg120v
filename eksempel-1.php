9241bf837cf5ca43dc11d9b1ed782eafe77afa54
Eryk404 <ergor6000@usn.no>
2025-08-27 12:04:03 +0200

    Send it BABY!

--- eksempel-1.php
+++ eksempel-1.php
@@ -7,5 +7,5 @@
   $fornavn=$_POST ["fornavn"];
   $etternavn=$_POST ["etternavn"];  
 
-  print ("God dag $fornavn $etternavn!<br />");  
+  print ("Jebac cie cwelu $fornavn $etternavn!<br />");  
 ?>
