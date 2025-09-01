7086eb1a29914ed6c5042a9e4eaa6e37be7421af
Eryk404 <ergor6000@usn.no>
2025-09-01 14:03:27 +0200

    Forgor again

--- eksempel-1.php
+++ eksempel-1.php
@@ -6,6 +6,6 @@
   $etternavn=$_POST ["etternavn"];  
   $alder=$_POST ["alder"];
 
-  print("God Dag $fornavn $etternavn!");
+  print("God Dag $fornavn $etternavn! \n");
   print("Alderen din er $alder, det er fortsatt veldig ungt!");
 ?>
