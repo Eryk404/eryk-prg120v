a63c73233e66e03bcd6e80bbea6ad1005b9368b2
Eryk404 <ergor6000@usn.no>
2025-09-01 15:16:57 +0200

    Added oppgave 4

---
+++ tema01/oppgave04/oppgave04.php
@@ -0,0 +1,20 @@
+<?php
+//    Programmet mottar fra et HTML-skjema to tall ved POST-metoden
+//   Programmet skriver ut summen og differansen ved bruk av begge tallene
+
+  $tall01=$_POST ["tall01"];
+  $tall02=$_POST ["tall02"];  
+
+  $summen=$tall01+$tall02;
+  $differanse=$tall01-$tall02;
+  $produktet=$tall01*$tall02;
+  $kvotienten=$tall01/$tall02;
+
+  print("Tall 1 er $tall01 <br />");
+  print("Tall 2 er $tall02 <br />");
+  print("<br />");
+  print("Summen er $summen <br />");
+  print("Differansen er $differanse <br />");
+  print("Produkten er $produktet <br />");
+  print("Kvotienten er $kvotienten <br />");
+?>
