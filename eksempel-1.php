67ee658993d394bced4f27f634c70cb710327f9a
Eryk404 <ergor6000@usn.no>
2025-09-01 13:52:17 +0200

    Test

--- eksempel-1.php
+++ eksempel-1.php
@@ -1,11 +1,10 @@
-<?php    /* Eksempel 1 */
-/*
-/*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
-/*    Programmet skriver ut en "god dag"-melding med personens navn 
-*/
+<?php
+//    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
+//   Programmet skriver ut en "god dag"-melding med personens navn 
 
   $fornavn=$_POST ["fornavn"];
   $etternavn=$_POST ["etternavn"];  
+  //$alder=$_POST ["alder"]
 
   print ("God Dag $fornavn $etternavn!");  
 ?>
