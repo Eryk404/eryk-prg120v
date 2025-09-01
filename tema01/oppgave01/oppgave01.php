9a1b01a052f3793d8f9c96c7fdc0220e41af513a
Eryk404 <ergor6000@usn.no>
2025-09-01 15:18:46 +0200

    Added oppgave 1

---
+++ tema01/oppgave01/oppgave01.php
@@ -0,0 +1,9 @@
+<?php
+//    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
+//   Programmet skriver ut en "god dag"-melding med personens navn 
+
+  $fornavn=$_POST ["fornavn"];
+  $etternavn=$_POST ["etternavn"];  
+
+  print("God Dag $fornavn $etternavn!");
+?>
