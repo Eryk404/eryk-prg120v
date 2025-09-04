19e15ca0c846f4430b8dec15eedf6e44621fc070
Eryk404 <ergor6000@usn.no>
2025-09-04 19:54:23 +0200

    Fixed?

--- tema02/oppgave01-t2/oppgave01-t2.php
+++ tema02/oppgave01-t2/oppgave01-t2.php
@@ -5,11 +5,11 @@
   $tall=$_POST ["tall"];
 
   if(tall == 9)
-  {
-   print("Riktig! 3 ganger 3 er 9")
-  }
+    {
+      print("Riktig! 3 ganger 3 er 9")
+    }
   else()
-  {
-    print("Feil... 3 ganger 3 er ikke $tall")
-  }
+    {
+      print("Feil... 3 ganger 3 er ikke $tall")
+    }
 ?>
