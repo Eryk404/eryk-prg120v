9b93dada8dd34d0d7c126b843bb8c9e9e6ac271c
Eryk404 <ergor6000@usn.no>
2025-09-04 19:55:12 +0200

    Forgor about ;

--- tema02/oppgave01-t2/oppgave01-t2.php
+++ tema02/oppgave01-t2/oppgave01-t2.php
@@ -6,10 +6,10 @@
 
   if(tall == 9)
     {
-      print("Riktig! 3 ganger 3 er 9")
+      print("Riktig! 3 ganger 3 er 9");
     }
   else()
     {
-      print("Feil... 3 ganger 3 er ikke $tall")
+      print("Feil... 3 ganger 3 er ikke $tall");
     }
 ?>
