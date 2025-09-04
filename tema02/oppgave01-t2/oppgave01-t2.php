7e3a7f6f2b4bb846e4e4a6cd6b6cb6790398a24a
Eryk404 <ergor6000@usn.no>
2025-09-04 19:57:02 +0200

    Fix

--- tema02/oppgave01-t2/oppgave01-t2.php
+++ tema02/oppgave01-t2/oppgave01-t2.php
@@ -2,13 +2,13 @@
 //    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
 //   Programmet skriver ut en "god dag"-melding med personens navn 
 
-  $tall=$_POST ["tall"];
+  $tall=$_POST["tall"];
 
-  if(tall == 9)
+  if($talltall == 9)
     {
       print("Riktig! 3 ganger 3 er 9");
     }
-  else()
+  else
     {
       print("Feil... 3 ganger 3 er ikke $tall");
     }
