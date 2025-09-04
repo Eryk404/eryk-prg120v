ac8a0b1ce1418ba09a34440c452a28798289a64a
Eryk404 <ergor6000@usn.no>
2025-09-04 20:28:08 +0200

    Added task 2 from tema02

---
+++ tema02/oppgave02-t2/oppgave02-t2.php
@@ -0,0 +1,21 @@
+<?php
+
+  $student=$_POST["student"];
+
+  if($student == j)
+    {
+      print("Du har svart ja på spørsmålet om du er student");
+    }
+  else if($student == n)
+    {
+      print("Du har svart nei på spørsmålet om du er student");
+    }
+  else if($student != j || != n)
+    {
+      print("Du har ikke svart ja eller nei på spørsmålet om du er student");
+    }
+  else
+    {
+      print("Du har ikke svart på spørsmålet om du er student");
+    }
+?>
