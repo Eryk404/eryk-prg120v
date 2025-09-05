8202671ff424e04653644bdd3b11a61976b8f7db
Eryk404 <ergor6000@usn.no>
2025-09-05 13:40:41 +0200

    Added <br/> to make it more legible

--- tema02/oppgave07-t2/oppgave07-t2.php
+++ tema02/oppgave07-t2/oppgave07-t2.php
@@ -10,41 +10,41 @@
 
   if ($tall01 == $tall02)
   {
-    print("Tall 1 er lik Tall 2");
+    print("Tall 1 er lik Tall 2 <br/>");
   }
   else if ($tall01 > $tall02)
   {
-    print("Tall 1 er større enn Tall 2");
+    print("Tall 1 er større enn Tall 2 <br/>");
   }
   else if ($tall01 < $tall02)
   {
-    print("Tall 1 er mindre enn Tall 2");
+    print("Tall 1 er mindre enn Tall 2 <br/>");
   }
 
   if ($tall01 == $tall03)
   {
-    print("Tall 1 er lik Tall 3");
+    print("Tall 1 er lik Tall 3 <br/>");
   }
   else if ($tall01 > $tall03)
   {
-    print("Tall 1 er større enn Tall 3");
+    print("Tall 1 er større enn Tall 3 <br/>");
   }
   else if ($tall01 < $tall03)
   {
-    print("Tall 1 er mindre enn Tall 3");
+    print("Tall 1 er mindre enn Tall 3 <br/>");
   }
 
   if ($tall02 == $tall03)
   {
-    print("Tall 2 er lik Tall 3");
+    print("Tall 2 er lik Tall 3 <br/>");
   }
   else if ($tall02 > $tall03)
   {
-    print("Tall 2 er større enn Tall 3");
+    print("Tall 2 er større enn Tall 3 <br/>");
   }
   else if ($tall02 < $tall03)
   {
-    print("Tall 2 er mindre enn Tall 3");
+    print("Tall 2 er mindre enn Tall 3 <br/>");
   }
   
 ?>
