3f736dc4b9ae40135f9a180f50883a3ff49b4891
Eryk404 <ergor6000@usn.no>
2025-09-05 13:39:02 +0200

    Added oppgave 7 from tema02

---
+++ tema02/oppgave07-t2/oppgave07-t2.php
@@ -0,0 +1,50 @@
+<?php
+
+  $tall01=$_POST["tall01"];
+  $tall02=$_POST["tall02"];
+  $tall03=$_POST["tall03"];
+
+  Print("Tall 1 er $tall01 <br/>")
+  Print("Tall 2 er $tall02 <br/>")
+  Print("Tall 3 er $tall03 <br/>")
+
+  if ($tall01 == $tall02)
+  {
+    print("Tall 1 er lik Tall 2");
+  }
+  else if ($tall01 > $tall02)
+  {
+    print("Tall 1 er større enn Tall 2");
+  }
+  else if ($tall01 < $tall02)
+  {
+    print("Tall 1 er mindre enn Tall 2");
+  }
+
+  if ($tall01 == $tall03)
+  {
+    print("Tall 1 er lik Tall 3");
+  }
+  else if ($tall01 > $tall03)
+  {
+    print("Tall 1 er større enn Tall 3");
+  }
+  else if ($tall01 < $tall03)
+  {
+    print("Tall 1 er mindre enn Tall 3");
+  }
+
+  if ($tall02 == $tall03)
+  {
+    print("Tall 2 er lik Tall 3");
+  }
+  else if ($tall02 > $tall03)
+  {
+    print("Tall 2 er større enn Tall 3");
+  }
+  else if ($tall02 < $tall03)
+  {
+    print("Tall 2 er mindre enn Tall 3");
+  }
+  
+?>
