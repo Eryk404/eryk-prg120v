dfd25561e7a6eddf847e2d8fc8b0d035980ccbdb
Eryk404 <ergor6000@usn.no>
2025-09-05 12:25:00 +0200

    New task 5 from tema02

---
+++ tema02/oppgave05-t2/oppgave05-t2.php
@@ -0,0 +1,61 @@
+<?php
+
+  $tall01=$_POST["tall01"];
+  $tall02=$_POST["tall02"];
+  $regneoperasjon=$_POST["regneoperasjon"];
+  $regneprint = 0;
+  $svar = 0;
+
+  if($regneoperasjon != 1 && !=2 && !=3 && != 4)
+  {
+    print("Feil regneoperasjons tall <br/>");
+  }
+  else
+  {
+    switch($regneoperasjon)
+    {
+      case 1:
+        $svar = $tall01 + $tall02;
+        break;
+      case 2:
+        $svar = $tall01 - $tall02;
+        break;
+      case 3:
+        $svar = $tall01 * $tall02;
+        break;
+      case 4:
+        $svar = $tall01 / $tall02;
+        break;
+    }
+
+  if(!$tall01 || !$tall02 || $regneoperasjon)
+    {
+      print("Du har ikke svart på spørsmålene");
+    }
+
+    if($regneoperasjon = 1)
+    {
+      $regneprint = "Addisjon";
+    }
+    elseif($regneoperasjon = 2)
+    {
+      $regneprint = "Subtraksjon";
+    }
+    elseif($regneoperasjon = 3)
+    {
+      $regneprint = "Multiplikasjon";
+    }
+    elseif($regneoperasjon = 4)
+    {
+      $regneprint = "Divisjon";
+    }
+
+    else
+    {
+      print("Tall 1 er $tall01 <br/>");
+      print("Tall 2 er $tall02 <br/>");
+      print("Regneoperasjon er $regneprint <br/>");
+      print("Resultatet av regneoperasjonen er $svar <br/>");
+    }
+  }
+?>
