c41eea8f9928d1ed4e2f18cdc22733254cbf4584
Eryk404 <ergor6000@usn.no>
2025-09-08 20:47:21 +0200

    To many ;

--- tema03/oppgave03-t3/oppgave03-t3.php
+++ tema03/oppgave03-t3/oppgave03-t3.php
@@ -1,17 +1,18 @@
 <?php
-for($tall = 1; $tall <=10; $tall++;)
+
+for($tall = 1; $tall <=10; $tall++)
 {
   print("$tall ");
 }
 print("<br/> ");
 
-for($tall = 11; $tall <=20; $tall++;)
+for($tall = 11; $tall <=20; $tall++)
 {
   print("$tall ");
 }
 print("<br/> ");
 
-for($tall = 21; $tall <= 30; $tall++;)
+for($tall = 21; $tall <= 30; $tall++)
 {
   print("$tall ");
 }
