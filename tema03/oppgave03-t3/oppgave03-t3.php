fb3d2c7df64f50f7132c66e21e83249da3e1b94b
Eryk404 <ergor6000@usn.no>
2025-09-08 20:38:00 +0200

    Had to simplify the code even more

--- tema03/oppgave03-t3/oppgave03-t3.php
+++ tema03/oppgave03-t3/oppgave03-t3.php
@@ -1,20 +1,17 @@
 <?php
-
-$tall = 1;
-
-for($tall<=10; $tall++;)
+for($tall = 1; $tall <=10; $tall++;)
 {
   print("$tall ");
 }
 print("<br/> ");
 
-for($tall<=20; $tall++;)
+for($tall = 11; $tall <=20; $tall++;)
 {
   print("$tall ");
 }
 print("<br/> ");
 
-for($tall<=30; $tall++;)
+for($tall = 21; $tall <= 30; $tall++;)
 {
   print("$tall ");
 }
