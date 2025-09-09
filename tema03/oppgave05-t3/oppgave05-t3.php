0da816f38430dfb8db64a50702f980d1b67500a5
Eryk404 <ergor6000@usn.no>
2025-09-09 12:27:37 +0200

    Fix

--- tema03/oppgave05-t3/oppgave05-t3.php
+++ tema03/oppgave05-t3/oppgave05-t3.php
@@ -7,7 +7,7 @@ for($tall = 1; $tall <=10; $tall++)
 {
   $sum = $sum + $tall;
 }
-$avg = $sum/$tall;
+$avg = $sum/($tall-1);
 print("Summen av tallene fra 1 til 10 er $sum <br/>");
 print("Gjennomsnittet av tallene fra 1 til 10 er $avg <br/>");
 ?>
