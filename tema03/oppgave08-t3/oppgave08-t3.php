0e608c9b1f7b7c92211f1c725e31124981c6b652
Eryk404 <ergor6000@usn.no>
2025-09-09 13:04:12 +0200

    Fix

--- tema03/oppgave08-t3/oppgave08-t3.php
+++ tema03/oppgave08-t3/oppgave08-t3.php
@@ -1,6 +1,6 @@
 <?php
 
-$tall1=$_POST ["tall2"];
+$tall1=$_POST ["tall1"];
 $tall2=$_POST ["tall2"];
 $tall3=$_POST ["tall3"];
 $tall4=$_POST ["tall4"];
@@ -8,14 +8,14 @@ $tall5=$_POST ["tall5"];
 
 $tallarray = [$tall1, $tall2, $tall3, $tall4, $tall5]
 
-for ($i = 0; $i <= 4; i++)
+for ($i = 0; $i <= 4; $i++)
 {
-  print($tallarray[i]);
+  print($tallarray[$i]);
 }
 
-for ($i = 4; $i >= 0; i--)
+for ($i = 4; $i >= 0; $i--)
 {
-  print($tallarray[i]);
+  print($tallarray[$i]);
 }
 
 ?>
