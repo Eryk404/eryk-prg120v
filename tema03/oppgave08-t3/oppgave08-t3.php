d3bf25f393ff26b56c8fd30ac924761041b4eed2
Eryk404 <ergor6000@usn.no>
2025-09-09 13:02:00 +0200

    Added oppgave 8 from tema03

---
+++ tema03/oppgave08-t3/oppgave08-t3.php
@@ -0,0 +1,21 @@
+<?php
+
+$tall1=$_POST ["tall2"];
+$tall2=$_POST ["tall2"];
+$tall3=$_POST ["tall3"];
+$tall4=$_POST ["tall4"];
+$tall5=$_POST ["tall5"];
+
+$tallarray = [$tall1, $tall2, $tall3, $tall4, $tall5]
+
+for ($i = 0; $i <= 4; i++)
+{
+  print($tallarray[i]);
+}
+
+for ($i = 4; $i >= 0; i--)
+{
+  print($tallarray[i]);
+}
+
+?>
