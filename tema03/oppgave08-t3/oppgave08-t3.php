5899b6024b681789a54bb6199eadd8a31c9a5bd2
Eryk404 <ergor6000@usn.no>
2025-09-09 13:05:26 +0200

    Fixed?

--- tema03/oppgave08-t3/oppgave08-t3.php
+++ tema03/oppgave08-t3/oppgave08-t3.php
@@ -6,7 +6,7 @@ $tall3=$_POST ["tall3"];
 $tall4=$_POST ["tall4"];
 $tall5=$_POST ["tall5"];
 
-$tallarray = [$tall1, $tall2, $tall3, $tall4, $tall5]
+$tallarray = [$tall1, $tall2, $tall3, $tall4, $tall5];
 
 for ($i = 0; $i <= 4; $i++)
 {
