fe629f1f8e1741f2586a3fda602a09a5c9381bb7
Eryk404 <ergor6000@usn.no>
2025-09-19 18:04:17 +0200

    Forgor about ;

--- tema04/oppgave01-t4/oppgave01-t4.php
+++ tema04/oppgave01-t4/oppgave01-t4.php
@@ -6,16 +6,16 @@ if ($postnummer)
   {
     if (ctype_digit($postnummer))
       if (strlen($postnummer) == 4)
-        print("$postnummer <br/>")
+        print("$postnummer <br/>");
       else{
-        print("Et postnummer må bestå av nøyaktig fire sifre.")
+        print("Et postnummer må bestå av nøyaktig fire sifre.");
       }
     else {
-      print ("Postnummeret må kun inneholde tall <br/>")
+      print ("Postnummeret må kun inneholde tall <br/>");
     }
   }
 else if (!$postnummer){
-  print ("Du må oppgi et postnummer <br/>")
+  print ("Du må oppgi et postnummer <br/>");
 }
 
 ?>
