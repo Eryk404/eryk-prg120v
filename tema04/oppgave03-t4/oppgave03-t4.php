1c423706ee01c9c5a2c3b4de96b29dbca189ad59
Eryk404 <ergor6000@usn.no>
2025-09-19 18:36:42 +0200

    Added oppgave 3 from tema 4

--- tema04/oppgave03-t4/oppgave03-t4.php
+++ tema04/oppgave03-t4/oppgave03-t4.php
@@ -1,21 +1,25 @@
 <?php
 
-$postnummer=$_POST["postnummer"];
+$emnekode = $_POST["emnekode"]; 
 
-if ($postnummer)
-  {
-    if (ctype_digit($postnummer))
-      if (strlen($postnummer) == 4)
-        print("$postnummer <br/>");
-      else{
-        print("Et postnummer må bestå av nøyaktig fire sifre.");
-      }
-    else {
-      print ("Postnummeret må kun inneholde tall <br/>");
+if (empty($emnekode)) {
+    print("Du må oppgi et emnekode <br/>");
+} else if (strlen($emnekode) == 7) {
+    $bokstav01 = $emnekode[0];
+    $bokstav02 = $emnekode[1];
+    $bokstav03 = $emnekode[2];
+    $tall01 = $emnekode[3];
+    $tall01 = $emnekode[4];
+    $tall01 = $emnekode[5];
+    $bokstav04 = $emnekode[6];
+
+    if (ctype_alpha($bokstav01) && ctype_alpha($bokstav02) && ctype_alpha($bokstav03) && ctype_alpha($bokstav04) && ctype_digit($tall01) && ctype_digit($tall02) && ctype_digit($tall03)) {
+        print("Emnekoden er $klassekode <br/>");
+    } else {
+        print("Et emnekoden må bestå av fire bokstaver og tre tall <br/>");
     }
-  }
-else if (!$postnummer){
-  print ("Du må oppgi et postnummer <br/>");
+} else {
+    print("Et emnekoden må bestå av nøyaktig tre bokstaver, tre tall og et bokstav som site tegn <br/>");
 }
 
 ?>
