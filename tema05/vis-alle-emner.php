723b09ddec6ce4bbe8f1227da23d0c8c74f203da
Eryk404 <ergor6000@usn.no>
2025-09-23 17:27:33 +0200

    Idk

--- tema05/vis-alle-emner.php
+++ tema05/vis-alle-emner.php
@@ -1,38 +1,34 @@
 <!DOCTYPE html>
 <html lang="no">
-    <head>
-
+<head>
     <!-- Metadata section for page settings and resources -->
     <meta charset="UTF-8"> <!-- Supports special characters like æ, ø, å -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vis alle emner</title>
-
-    </head>
-
-</html>
-
+</head>
+<body>
 <?php 
 
 include("db-tilkobling.php");
 
-$sqlSetning="SELECT * FROM emne ORDER BY emnekode;";
-$sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig å hente data fra databasen");
-$antallRader=mysqli_num_rows($sqlResultat);
+$sqlSetning = "SELECT * FROM emne ORDER BY emnekode;";
+$sqlResultat = mysqli_query($db, $sqlSetning) or die("ikke mulig å hente data fra databasen");
+$antallRader = mysqli_num_rows($sqlResultat);
 
-print ("<h3>Registrerte emner </h3>");
-print ("<table border=1>");
-print ("<tr><th align=left>emnekode</th> <th align=left>emnenavn</th> </tr> <th align=left>studiumkode</th>");
+print("<h3>Registrerte emner</h3>");
+print("<table border=1>");
+print("<tr><th align=left>emnekode</th><th align=left>emnenavn</th><th align=left>studiumkode</th></tr>");
 
-
-for ($r=1;$r<=$antallRader;$r++)
-{
-    $rad=mysqli_fetch_array($sqlResultat); /* ny rad hentet fra spørringsresultatet */
-    $emnekode=$rad["emnekode"];
-    $emnenavn=$rad["emnenavn"];
-    $studiumkode=$rad["studiumkode"];
-    print ("<tr> <td> $emnemkode </td> <td> $emnenavn </td> </tr> $studiumkode </td> </tr>");
+for ($r = 1; $r <= $antallRader; $r++) {
+    $rad = mysqli_fetch_array($sqlResultat);
+    $emnekode = $rad["emnekode"];
+    $emnenavn = $rad["emnenavn"];
+    $studiumkode = $rad["studiumkode"];
+    print("<tr><td>$emnekode</td><td>$emnenavn</td><td>$studiumkode</td></tr>");
 }
 
-print ("</table>");
-
+print("</table>");
 ?>
+
+</body>
+</html>
